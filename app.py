c56cae24271af60211c7fc5b292ae4cf7d08a421
DeadQueen <aleks20aleks0228@gmail.com>
2024-03-01 18:41:39 +0300

    Extend index.html to be code generated

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader
 app = Flask(__name__)
+env = Environment(loader=FileSystemLoader(f'{app.template_folder}/'))
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    temp = env.get_template("index.html")
+    return temp.render(data_source=[
+            {
+                "title": dir_name,
+                "image_sources": [
+                    f'static/Pages/{dir_name}/image_names/' + link
+                    for link in os.listdir(f'static/Pages/{dir_name}/image_names')
+                ],
+            }
+            for dir_name in os.listdir('static/Pages')
+        ])
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def sub_directory(path):
+    if path + '.html' in os.listdir(f'./{app.template_folder}'):
+        return render_template(path + '.html')
+
+    return render_template('error.html')
 
 
 if __name__ == '__main__':
