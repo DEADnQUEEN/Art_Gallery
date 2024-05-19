2bb0746639e60f33d9ae065679e8122566724f1c
User2 <aleks20aleks0228@gmail.com>
2024-05-20 02:23:41 +0300

    Added a few pages + small UI fix

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import string
+import types
 import werkzeug.exceptions
 from werkzeug.wrappers.response import HTTPStatus
-from flask import Flask, redirect, abort, url_for
+from flask import Flask, redirect, abort, request
 from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
@@ -20,7 +21,27 @@ def data_compose(path: str) -> dict[str: str or dict or list]:
     ls = os.listdir(path)
     sub = '\\'
     extents = ['.css', '.js']
-    data: dict[str: str or dict] = {
+
+    def text_extend(file_path: str) -> dict:
+        out = {
+            'title': '',
+            'subtitle': '',
+            'content': '',
+            'footer': '',
+        }
+
+        with open(file_path[3:], 'r', encoding='utf-8') as text:
+            lines = text.readlines()
+            for line in range(len(lines)):
+                out[list(out.keys())[line]] = lines[line][:-1]
+
+        return out
+
+    func_extents: dict[str: types.FunctionType] = {
+        '.txt': text_extend
+    }
+
+    data: dict[str: str or dict or list or int] = {
         "title": path.split(sub)[-1],
         "dirs": {},
         "content": [],
@@ -30,18 +51,28 @@ def data_compose(path: str) -> dict[str: str or dict or list]:
     for i in range(len(extents)):
         data[extents[i][1:]] = {}
 
+    for key in func_extents.keys():
+        data[key[1:]] = []
+
     for i in range(len(ls)):
         pth = "..\\static" + (f"{path}\\{ls[i]}"[len(app.static_folder):])
         if os.path.isdir(f"{path}/{ls[i]}"):
             data["dirs"][ls[i]] = data_compose(f"{path}\\{ls[i]}")
+
         else:
             for ex in range(len(extents)):
                 if extents[ex] in ls[i]:
                     data[extents[ex][1:]][ls[i].rstrip(string.ascii_letters)[:-1]] = pth
-                    print(pth)
                     break
             else:
-                data["content"].append(pth)
+                for key in func_extents.keys():
+                    if key in ls[i]:
+                        data[key[1:]].append(func_extents[key](pth))
+                        break
+                else:
+                    data["content"].append(pth)
+
+    data['blocks'] = min(len(data['content']), len(data['txt']))
 
     return data
 
@@ -60,40 +91,44 @@ def main():
     return redirect('Gallery', Response=None, code=HTTPStatus.FOUND)
 
 
-@app.route('/Gallery')
+@app.route('/Gallery', methods=['GET'])
 def gallery():
     return create_template('Gallery')
 
 
-@app.route('/test')
-def test():
-    return env.get_template('test.html').render()
+@app.route('/Abstract', methods=['GET'])
+def abstract():
+    return create_template('Abstract')
 
 
-def error_template(error: HTTPStatus):
-    data = data_compose(f'{app.static_folder}\\Error')
-    data['code'] = error.value
-    data['description'] = error.description
-    template = env.get_template(data['html'])
-    return template.render(data=data)
+@app.route('/Minimalism', methods=['GET'])
+def minimalism():
+    return create_template('Minimalism')
 
 
-@app.route('/Error<int:e>')
-def error_load(e: int):
-    error: HTTPStatus
-    for state in HTTPStatus:
-        if state.value == e:
-            error = state
-            break
-    else:
-        error = HTTPStatus.NOT_FOUND
+@app.route('/Modern', methods=['GET'])
+def modern():
+    return create_template('Modern')
 
-    return error_template(error)
+
+@app.route('/Portrait', methods=['GET'])
+def portrait():
+    return create_template('Portrait')
+
+
+@app.route('/About', methods=['GET'])
+def about():
+    return create_template('About')
 
 
 @app.errorhandler(werkzeug.exceptions.HTTPException)
 def exc(e: werkzeug.exceptions.HTTPException):
-    return redirect(url_for('error_load', e=e.code), Response=None, code=HTTPStatus.FOUND)
+    data = data_compose(f'{app.static_folder}\\Error')
+    data['code'] = e.code
+    data['description'] = e.description
+    template = env.get_template(data['html'])
+
+    return template.render(data=data)
 
 
 if __name__ == '__main__':
--- static/Pages/Gallery/app.js
+++ static/Pages/Gallery/app.js
@@ -1,8 +1,20 @@
-document.querySelector('.scroll').addEventListener('wheel', (e) => {
-    e.preventDefault();
-  
-    const container = document.querySelector('.scroll');
-    container.scrollTo({
-      left: container.scrollLeft + e.deltaY,
+let d = document.getElementsByClassName('scroll');
+let html = document.getElementsByTagName('html')[0];
+
+for (let i = 0; i < d.length; i++){
+    d[i].addEventListener('wheel', (e) =>{
+        e.preventDefault();
+        if (d[i].scrollLeft + e.deltaY > 0 && d[i].scrollLeft + e.deltaY < d[i].scrollWidth - d[i].clientWidth) 
+        {
+            d[i].scrollTo({
+                left: d[i].scrollLeft + e.deltaY + e.deltaX
+            });
+        }
+        else
+        {
+            html.scrollTo({
+                top: html.scrollTop + e.deltaY
+            })
+        }
     });
-  });
+}
